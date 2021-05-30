895f60d3472a0e1d7e664a10dfe8a2901079dae6
Cyrus <oncwnuCdOBr1R9X-A-r4R0NABk2Y@git.weixin.qq.com>
2021-05-30 11:24:12 +0800

    vue3模板

---
+++ vue3_template.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="自定义">
+    <h2>自定义内容</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "自定义",
+    components:{},
+    props:[],
+    data(){
+      return {
+          
+      }
+    },
+    setup(){},
+    methods:{},
+    computed:{},
+    watch:{},
+    // 声明周期函数
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    beforeUnmount(){},
+    unmounted(){},
+    // 使用keep-alive生效
+    activated() {},
+    deactivated() {}
+  }
+</script>
+<style lang='less'>
+  .自定义 {
+    display: block;
+};
+</style>
